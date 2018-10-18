3de4d5e9eb847b69c11492f87b81fd435e6fe7db
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-10-18 23:19:34 +0200

    :construction: Print Bitfinex BTC/USD price

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('wss://stream.binance.com:9443/ws/BTC/USDT@miniTicker');
+// const ws = new WebSocket('wss://stream.binance.com:9443/ws/BTC/USDT@miniTicker');
+
+// ws.on('message', (e) => {
+//   let { c: price } = JSON.parse(e);
+
+//   if (!price) {
+//     return;
+//   }
+
+//   price = parseFloat(c);
+//   price = price.toFixed(2);
+
+//   console.log(price);
+// });
+
+const ws = new WebSocket('wss://api.bitfinex.com/ws/2');
+
+ws.on('open', () => ws.send(
+  JSON.stringify({
+    event: 'subscribe',
+    channel: 'ticker',
+    symbol: 'tBTCUSD'
+  })
+));
 
 ws.on('message', (e) => {
-  let { c: price } = JSON.parse(e);
+  let price = JSON.parse(e);
 
-  if (!price) {
+  if (!Array.isArray(data[1])) {
     return;
   }
 
-  price = parseFloat(c);
+  price = parseFloat(data[1][6]);
   price = price.toFixed(2);
 
   console.log(price);
