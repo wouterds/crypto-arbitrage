c46f3f9dfccbc3e6d6a8410d678b5a6e285e8777
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-10-19 01:23:52 +0200

    :sparkles: Print prices + arb

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+const _ = require('lodash');
 const BinanceTicker = require('./tickers/binance');
 const CoinbaseProTicker = require('./tickers/coinbase-pro');
 const BitfinexTicker = require('./tickers/bitfinex');
@@ -11,11 +13,21 @@ const start = () => {
   setTimeout(() => coinbaseProTicker.start(), 1);
   setTimeout(() => bitfinexTicker.start(), 1);
 
-  setInterval(() => console.log({
-    'Binance': binanceTicker.price,
-    'Coinbase Pro': coinbaseProTicker.price,
-    'Bitfinex': bitfinexTicker.price,
-  }), 1000);
+  setInterval(() => {
+    let binance = binanceTicker.price;
+    let coinbasePro = coinbaseProTicker.price;
+    let bitfinex = bitfinexTicker.price;
+
+    const max = _.max([binance, coinbasePro, bitfinex]);
+    const min = _.min([binance, coinbasePro, bitfinex]);
+    const arb = max - min;
+
+    binance = binance === max ? chalk.green(binance.toFixed(2)) : (binance === min ? chalk.red(binance.toFixed(2)) : chalk.whiteBright(binance.toFixed(2)));
+    coinbasePro = coinbasePro === max ? chalk.green(coinbasePro.toFixed(2)) : (coinbasePro === min ? chalk.red(coinbasePro.toFixed(2)) : chalk.whiteBright(coinbasePro.toFixed(2)));
+    bitfinex = bitfinex === max ? chalk.green(bitfinex.toFixed(2)) : (bitfinex === min ? chalk.red(bitfinex.toFixed(2)) : chalk.whiteBright(bitfinex.toFixed(2)));
+
+    console.log(`${chalk.white('Binance:')} ${binance} ${chalk.white('Coinbase Pro:')} ${coinbasePro} ${chalk.white('Bitfinex:')} ${bitfinex} - ${chalk.white('arbitrage:')} ${chalk.yellowBright(arb.toFixed(2))}`);
+  }, 1000);
 
   process.stdin.resume();
 };
