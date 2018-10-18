c91a2bffa556e440312cb5885cabf79ec63a86d5
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-10-18 23:56:59 +0200

    :construction: Print Coinbase Pro BTC/USD price

--- index.js
+++ index.js
@@ -15,24 +15,46 @@ const WebSocket = require('ws');
 //   console.log(price);
 // });
 
-const ws = new WebSocket('wss://api.bitfinex.com/ws/2');
+// const ws = new WebSocket('wss://api.bitfinex.com/ws/2');
+
+// ws.on('open', () => ws.send(
+//   JSON.stringify({
+//     event: 'subscribe',
+//     channel: 'ticker',
+//     symbol: 'tBTCUSD'
+//   })
+// ));
+
+// ws.on('message', (e) => {
+//   let price = JSON.parse(e);
+
+//   if (!Array.isArray(price[1])) {
+//     return;
+//   }
+
+//   price = parseFloat(price[1][6]);
+//   price = price.toFixed(2);
+
+//   console.log(price);
+// });
+
+const ws = new WebSocket('wss://ws-feed.pro.coinbase.com');
 
 ws.on('open', () => ws.send(
   JSON.stringify({
-    event: 'subscribe',
-    channel: 'ticker',
-    symbol: 'tBTCUSD'
+    type: 'subscribe',
+    channels: [{ name: 'ticker', product_ids: ['BTC-USD'] }]
   })
 ));
 
 ws.on('message', (e) => {
-  let price = JSON.parse(e);
+  let { price } = JSON.parse(e);
 
-  if (!Array.isArray(data[1])) {
+  if (!price) {
     return;
   }
 
-  price = parseFloat(data[1][6]);
+  price = parseFloat(price);
   price = price.toFixed(2);
 
   console.log(price);
